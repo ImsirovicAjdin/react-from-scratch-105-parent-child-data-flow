a25781c6623d99d9d4f2db92d92e26882a281c8f
Ajdin Imsirovic <ajdika@live.com>
2022-06-30 12:44:29 +0200

    (2) Props values in the parent can come from variables

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Headings from "./Headings";
 
 function App() {
+  const h1 = "Hello World!"
+  const h2 = "Hello again!"
+  const h3 = "Hello yet again!"
+  const h4 = "Once again!"
   return (
     <div className="App">
       <Headings
-        h1="Hello World!"
-        h2="Hello again!"
-        h3="Hello yet again!"
-        h4="Once again!"
-      />
+        h1={h1}
+        h2={h2}
+        h3={h3}
+        h4={h4}
+    />
     </div>
   )
 }
